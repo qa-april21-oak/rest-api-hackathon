4d2f85dd6048356f562659352cf95cbe3baa2404
Oliver <starlight.dev@hotmail.com>
2021-06-25 14:28:46 +0100

    OD: Added Get by id and create REST Endpoint

--- routes.js
+++ routes.js
@@ -2,7 +2,30 @@ const express = require("express");
 
 const router = express.Router();
 
+const Film = require("./film")
 // routes go here
+
+router.post("/film", async (req, res) => {
+	const film = new Film({
+		// TODO
+	})
+
+	await film.save();
+
+	res.send(film);
+})
+
+router.get("/film/:id", async(req, res) => {
+	
+	try {
+		const film = await Film.findById(req.params.id);
+		res.send(prod);
+
+	} catch {
+		res.status(404).send("No film found");
+	}
+
+})
 
 
 module.exports = router;
