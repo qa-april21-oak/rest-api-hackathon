71a761eacbcb1727f96af14250cd12838167bccd
JOwolabi <jowolabi@qa.com>
2021-06-25 14:59:33 +0100

    JO- films improvements after the merge

--- models/film.js
+++ models/film.js
@@ -1,14 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-//Creation of the film schema
-const movieSchema = new Schema({
-    title: String,
-    description: String,
-    dateRelease: Date,
-});
 
-// Validation of the Schema
+
+// Creation of the Validation of the Schema
 const movieSchema = new Schema({
     title: {
         type: String,
