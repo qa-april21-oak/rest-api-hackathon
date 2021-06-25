3f9784313e19d8f5257154291b5e89a14b2effea
Oliver <starlight.dev@hotmail.com>
2021-06-25 14:51:25 +0100

    OD: Fixed trailing {

--- routes.js
+++ routes.js
@@ -5,9 +5,51 @@ const router = express.Router();
 const Film = require("./models/film")
 // routes go here
 
+    try {
+        const film = await Films.findById(req.params.id);
+        film.title = req.body.title
+        film.description = req.body.description
+        film.dateRelease = req.body.dateRelease
+        film.filmTime = req.body.filmTime
+        film.save();
+        res.send(film);
+
+    } catch {
+        res.status(404);
+        res.send({ error: "Database error Film did not updated." })
+    }
+})
+
+//Patch Update Many - EB
+router.patch('/films/:id', async (req, res) => {
+    try {
+        const film = await Films.findById(req.params.id);
+        film.title = req.body.title || film.title
+        film.description = req.body.description || film.description
+        film.dateRelease = req.body.dateRelease || film.dateRelease
+        film.filmTime = req.body.filmTime || film.filmTime
+        film.save();
+        res.send(film);
+    } catch {
+        res.status(404);
+        res.send({ error: "Database error Film did not updated. (Film not found)" })
+    }
+})
+
+
+// Delete - EB
+router.delete('/films/:id', async (req, res) => {
+    try {
+        const film = await Films.findByIdAndRemove(req.params.id);
+        res.send(film);
+    } catch {
+        res.status(404);
+        res.send({ error: "Film doesn't exist" })
+    }
+});
+
 router.post("/film", async (req, res) => {
 	const film = new Film({
-		// TODO
 		title: req.body.title,
 		description: req.body.description,
 		dateRelease: req.body.dateRelease,
