4d54098b7ee228a288a63b004a73b2ffd7fcdd6d
Gwoxenox <gwendolen.old@gmail.com>
2021-06-25 14:44:49 +0100

    added schema for actors

--- models/dataSchema.js
+++ models/dataSchema.js
@@ -18,3 +18,24 @@ const reviews = new Schema({
         max: 5
     }
 })
+
+const actors = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    surname: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    gender: {
+        type: String,
+        enum: ["Male", "Female", "Other", null]
+    },
+    dateOfBirth: Date,
+    movies: [{
+        type: String
+    }]
+})
