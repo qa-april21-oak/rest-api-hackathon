efce568e6c45ae999760d0913dad00e69607b7f9
JOwolabi <jowolabi@qa.com>
2021-06-25 14:43:31 +0100

    JO- improvements to the flim schema

--- models/film.js
+++ models/film.js
@@ -1,9 +1,30 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//Creation of the film schema
 const movieSchema = new Schema({
     title: String,
     description: String,
     dateRelease: Date,
+});
+
+// Validation of the Schema
+const movieSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        minlength: 2
+    },
+    description: {
+        type: String,
+        required: true,
+        minlength: 2
+    },
+    dateRelease: {
+        type: Date,
+        required: true,
+        min: 1940 - 01 - 01,
+        max: 2022 - 01 - 01
+    }
 });
 
